2a6b2251ebc7564308ce2681778a5e7cf468cbdf
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-13 20:09:50 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -5,7 +5,7 @@ const customers = [
     songs: [
       {
         title: "50 NĂM VỀ SAU",
-        wavLink: "https://1drv.ms/u/c/71fd2a7d74762b99/IQBn_pMgNS3xQ5gWsGJPPp9FAcHd9udieX0tVMkc23nsTM0?download=1"
+        wavLink: "https://drive.google.com/file/d/1EdX8wvk5CCssk4jYrafY066507A80ZVw/view?usp=sharing"
       }
     ]
   }
