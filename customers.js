26a533976f9fdbbf92ca9328115e388629cc3235
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-14 18:35:08 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -9,4 +9,25 @@ const customers = [
       }
     ]
   }
+
+
+
+const customers = [
+  {
+    id: "hoanguyen",
+    name: "Hoàng Uyên",
+    songs: [
+      {
+        title: "MÀU HOA BÍ",
+        wavLink: "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20UYEN%2FMAU%20HOA%20BI%20%2D%20HOANG%20UYEN%20%2D%20MASTER%203%2Ewav"
+      }
+    ]
+  }
+
+
+
+
+
+
+  
 ];
