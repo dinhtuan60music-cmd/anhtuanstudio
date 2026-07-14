6c09fb61f0e8d1415d819afe4e407ea78d12790d
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-14 18:59:34 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -16,7 +16,7 @@ const customers = [
     songs: [
       {
         title: "MÀU HOA BÍ",
-        wavLink: "https://my.microsoftpersonalcontent.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?UniqueId=396c7d56-227a-49ad-bb8d-ba6a2324c566&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJkODdhOWNkNi0wM2M2LTQ0MWQtOWQ2YS00NzFmODMyZjVmZTAiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzg0MDMzNzY4In0.slwi2CxrX2AHO8Ua0yVi0b9I5LGmH8KqCsxA7dIRG_8Y4V5E6FmYYH5vaRrT21zB_BAjSq1ZmpZVI5gqNdP22gYI-Kt6MygTwtNz4gO8DkUVGqWkvykrXAd58v_Zt1jp3iI_4VtXsOCl-mci6fnP97ItEiD2oTEp1KP6NWwR2LTcxL2lWwXM9-_eM9Z_DOVpSKojvQfZJdZLePX3lRkw8fiIwCDyMaos7qFdWk5FSXKyQXoHl86jZeFlQ4iwISsx17wekeLwj8S_Osz_z9InSifo0XaAML3srq4KDbRMVHDVP5QWUeQABPW4_iVcMwHyqIdqjFgtzhhytSlLxM3gnnVxQtP7qXkgg-OVdaHSexaXs4RnoABLK6qTxL4v0_4SLSwfxJoG1c_Yj6urxUbWKOevi5Y6eX5NmIVUEuS8b41UgT6R5zc5ySEYu_XGr57vg3YIsv87YUS35GYktWEVq7jqus5k8h_BPL6NlvTv1PA2XpMz4lPVmz03gA1ygaKTg_FXJn_O5YoBicQHwbS3IblvCifgPpFDs3GxVqAAHhQeMU_A-kB5Sm5Rtrv-warfpNGzc3yL4QNgyYCsscbMdg6AfBy6QzbU9zvT3v7QorM.lFv0stgNcdoUEtOcf1AYCPVcd2R2rZ4BtIfmTV2jHBc&ApiVersion=2.0"
+        wavLink: "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20UYEN%2FMAU%20HOA%20BI%20%2D%20HOANG%20UYEN%20%2D%20MASTER%203%2Ewav"
       }
     ]
   }
