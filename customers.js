de9ca501399e23f4e696d7654416a6613b907180
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-14 19:51:42 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -5,7 +5,12 @@ const customers = [
     songs: [
       {
         title: "50 NĂM VỀ SAU",
-        wavLink: "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20TRIEU%2F50%20NAM%20VE%20SAU%20%2D%20MASTER%2Ewav"
+
+        listenLink:
+          "https://dl.ithcm.vn/TEMP/50%20NAM%20VE%20SAU%20-%20MASTER.wav",
+
+        downloadLink:
+          "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20TRIEU%2F50%20NAM%20VE%20SAU%20%2D%20MASTER%2Ewav"
       }
     ]
   },
@@ -16,12 +21,14 @@ const customers = [
     songs: [
       {
         title: "MÀU HOA BÍ",
-        wavLink: "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20UYEN%2FMAU%20HOA%20BI%20%2D%20HOANG%20UYEN%20%2D%20MASTER%203%2Ewav"
+
+        listenLink: "https://1drv.ms/u/c/71fd2a7d74762b99/IQBWfWw5eiKtSbuNumojJMVmAfDK3z4TB56Uh1Qb8-Ck8VY?e=0mKJjb",
+
+        downloadLink:
+          "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20UYEN%2FMAU%20HOA%20BI%20%2D%20HOANG%20UYEN%20%2D%20MASTER%203%2Ewav"
       }
     ]
   }
-
-
 
 
 
