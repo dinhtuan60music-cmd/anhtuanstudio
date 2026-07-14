a39daef31b382eff4de76af7a3382ace55baa454
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-14 20:14:19 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -25,7 +25,7 @@ const customers = [
         listenLink: "https://1drv.ms/u/c/71fd2a7d74762b99/IQBWfWw5eiKtSbuNumojJMVmAfDK3z4TB56Uh1Qb8-Ck8VY?e=0mKJjb",
 
         downloadLink:
-          "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20UYEN%2FMAU%20HOA%20BI%20%2D%20HOANG%20UYEN%20%2D%20MASTER%203%2Ewav"
+          "https://drive.google.com/file/d/1HynvKbu1HVij5E5VtiIXoIjUSMWNhBF_/view?usp=sharing"
       }
     ]
   }
