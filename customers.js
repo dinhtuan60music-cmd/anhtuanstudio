069a6d0ea2165f7ec4cb440ea068aeaf84c9c975
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-14 18:45:01 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -8,11 +8,8 @@ const customers = [
         wavLink: "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20TRIEU%2F50%20NAM%20VE%20SAU%20%2D%20MASTER%2Ewav"
       }
     ]
-  }
+  },
 
-
-
-const customers = [
   {
     id: "hoanguyen",
     name: "Hoàng Uyên",
