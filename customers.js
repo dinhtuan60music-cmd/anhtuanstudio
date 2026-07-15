d1ba52c8791c7440ec7f3ca0b00800cb7350f2eb
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-15 20:12:29 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -7,7 +7,7 @@ const customers = [
       {
         title: "50 NĂM VỀ SAU",
         featured: true,
-priority: 2,
+priority: 1,
 
         listenLink:
           "https://dl.ithcm.vn/TEMP/50%20NAM%20VE%20SAU%20-%20MASTER.wav",
@@ -25,7 +25,7 @@ priority: 2,
       {
         title: "MÀU HOA BÍ",
            featured: true,
-priority: 1,
+priority: 2,
 
         listenLink: "https://drive.google.com/file/d/1eLSSLI6Ee0yx413nwXHpj3ccQfyy7EqD",
 
