fedb9aa905e4f4351e2914982acfae36c6b3beca
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-15 20:14:17 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -13,7 +13,7 @@ priority: 1,
           "https://dl.ithcm.vn/TEMP/50%20NAM%20VE%20SAU%20-%20MASTER.wav",
 
         downloadLink:
-          "https://onedrive.live.com/personal/71fd2a7d74762b99/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F71fd2a7d74762b99%2FDocuments%2FSTUDIO%2FKHACH%2FHOANG%20TRIEU%2F50%20NAM%20VE%20SAU%20%2D%20MASTER%2Ewav"
+          "https://ol.ithcm.vn/@s/50namvesau"
       }
     ]
   },
