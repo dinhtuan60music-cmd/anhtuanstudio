9bdcd34cdf4c75486d12f0404b92ff85243850e4
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-15 21:18:11 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -3,11 +3,12 @@ const customers = [
     id: "thanhsang",
     name: "Thanh Sáng",
     avatar: "avatar/thanhsang.png",
+
     songs: [
       {
         title: "50 NĂM VỀ SAU",
         featured: true,
-priority: 99,
+        priority: 99,
 
         listenLink:
           "https://dl.ithcm.vn/TEMP/50%20NAM%20VE%20SAU%20-%20MASTER.wav",
@@ -18,18 +19,19 @@ priority: 99,
     ]
   },
 
-
-  
   {
     id: "hoanguyen",
     name: "Hoàng Uyên",
+    avatar: "logo.png",
+
     songs: [
       {
         title: "MÀU HOA BÍ",
-           featured: true,
-priority: 1,
+        featured: true,
+        priority: 1,
 
-        listenLink: "https://raw.githubusercontent.com/dinhtuan60music-cmd/anhtuanstudio/81490acbd7f5e53e5eebed6e0854e2a15870a03f/files/MAU%20HOA%20BI%20-%20HOANG%20UYEN%20-%20MASTER.mp3",
+        listenLink:
+          "https://raw.githubusercontent.com/dinhtuan60music-cmd/anhtuanstudio/81490acbd7f5e53e5eebed6e0854e2a15870a03f/files/MAU%20HOA%20BI%20-%20HOANG%20UYEN%20-%20MASTER.mp3",
 
         downloadLink:
           "https://drive.google.com/file/d/1eLSSLI6Ee0yx413nwXHpj3ccQfyy7EqD/view?usp=sharing"
@@ -40,16 +42,19 @@ priority: 1,
   {
     id: "laudamus2025",
     name: "Nhóm Laudamus 2025",
+    avatar: "logo.png",
+
     songs: [
       {
         title: "TRÁI TIM KHÁT MONG",
-           featured: true,
-priority: 2,
+        featured: true,
+        priority: 2,
 
-        listenLink: "https://raw.github.com/dinhtuan60music-cmd/anhtuanstudio/blob/3ffd1e6ddecc65d51df8c812701802a1ae12a622/files/TRAI%20TIM%20KHAT%20MONG%20-%20MASTER%202.mp3",
+        listenLink:
+          "https://raw.githubusercontent.com/dinhtuan60music-cmd/anhtuanstudio/3ffd1e6ddecc65d51df8c812701802a1ae12a622/files/TRAI%20TIM%20KHAT%20MONG%20-%20MASTER%202.mp3",
 
         downloadLink:
-          "https://github.com/dinhtuan60music-cmd/anhtuanstudio/blob/3ffd1e6ddecc65d51df8c812701802a1ae12a622/files/TRAI%20TIM%20KHAT%20MONG%20-%20MASTER%202.mp3"
+          "https://github.com/dinhtuan60music-cmd/anhtuanstudio/raw/3ffd1e6ddecc65d51df8c812701802a1ae12a622/files/TRAI%20TIM%20KHAT%20MONG%20-%20MASTER%202.mp3"
       }
     ]
   },
