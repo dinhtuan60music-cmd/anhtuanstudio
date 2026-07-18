b377488160c7a90eb11f2415b230f41d217fd7f3
dinhtuan60music-cmd <dinhtuan60music@gmail.com>
2026-07-18 15:28:06 +0700

    Update customers.js

--- customers.js
+++ customers.js
@@ -61,21 +61,6 @@ const customers = [
     ]
   },
 
-/* Bài hát lẻ */
-      {
-        title: "Màu hoa dâm bụt",
-        featured: true,
-        priority: 3,
-
-        listenLink:
-          "https://raw.githubusercontent.com/dinhtuan60music-cmd/anhtuanstudio/81490acbd7f5e53e5eebed6e0854e2a15870a03f/files/MAU%20HOA%20BI%20-%20HOANG%20UYEN%20-%20MASTER.mp3",
-
-        downloadLink:
-          "https://drive.google.com/file/d/1eLSSLI6Ee0yx413nwXHpj3ccQfyy7EqD/view?usp=sharing"
-      }
-    ]
-  },
-
 
 /* ALBUM KHÁCH HÀNG MỚI */
   {
@@ -98,13 +83,11 @@ const customers = [
           ""
       }
     ]
-  },
+  }
 
 
 
 
-  
-  
 ];
 
 window.customers = customers;
